647a44ed22cdf3612bea7b70a140c1817a6c8be9
Ankur Sharma <ankur.101045@gmail.com>
2021-06-04 01:26:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const Cors = require('cors');
+const axios = require('axios');
+const express = require("express");
+const app = express();
+app.use(Cors())
+let port = process.env.PORT || 3005;
+app.get("/fetchSlots/:pin/:date", (req, res) => {
+  const options = {
+    headers: { 'Accept': 'application/json' }
+  }
+  let pin = req.params.pin
+  let selectedDate = req.params.date
+  axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pin}&date=${selectedDate}`, options).then(response => {
+    res.send(response.data)
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Example app is listening on port http://localhost:${port}`)
+});
